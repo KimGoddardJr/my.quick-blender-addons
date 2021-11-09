53b39e15eb5526afe14dbd7f3b5da072101c3282
Ugly Mountain <paxigalaxi@gmail.com>
2021-11-09 23:29:44 +0100

    positioning

--- normal_to_bone.py
+++ normal_to_bone.py
@@ -4,47 +4,117 @@ import math
 import numpy as np
 
 
-# get the index of the selected vertex or face
+#Blender Enum Widget showing all available Armatures in the scene
+#Blender Menu Tab with bl_options
+class ArmatureSelector(bpy.types.MenuItem):
+    bl_idname = "OBJECT_MT_armature_selector"
+    bl_label = "Armature Selector"
 
-ob = bpy.context.object
-count = len(ob.data.vertices)
-sel = np.zeros(count, dtype=np.bool)
-
-ob.data.vertices.foreach_get('select', sel)
-
-print(sel)
+    def draw(self, context):
+        layout = self.layout
+        layout.operator_context = 'INVOKE_DEFAULT'
+        layout.operator(self.ArmatureLister.bl_idname, text="Armature List")
+        layout.operator(ArmatureSelector.bl_idname, text="Armature Selector")
 
 
-# get the normal of selected vertex or face or edge
-def get_normal(obj, mode):
-    # Get the selected object
-    me = obj.data
-    print(me)
-    # Get the selected mode
-    if mode == 'VERTEX':
-        # Get the selected vertex index
-        index = obj.data.vertices.active_index
-        # index = obj.data.vertices.active.index
-        # Get the normal of the selected vertex
-        normal = me.vertices[index].normal
-    elif mode == 'FACE':
-        # Get the selected face
-        index = me.polygons.active.index
-        # Get the normal of the selected face
-        normal = me.polygons[index].normal
-        print(normal)
-    elif mode == 'EDGE':
-        # Get the selected edge
-        index = me.edges.active.index
-        # Get the normal of the selected edge
-        normal = me.edges[index].normal
-    # Return the normal
+    def ArmatureLister(self):
+        # Get the list of all Armatures in the scene
+        armature_list = bpy.data.armatures
+        # Get the list of all Armatures in the scene
+        for armature in armature_list:
+            print(armature.name)
 
-# Convert the normal of a vertex to a rotational value
-def normal_to_bone(normal):
-    # Get the angle between the normal and the z-axis
-    angle = math.atan2(normal.y, normal.x)
-    # Convert the angle to a value between 0 and 1
-    bone_value = angle / (2 * math.pi)
-    # Return the bone value
-    return bone_value
+class NormalToBone(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.normal_to_bone"
+    bl_label = "Normal to Bone"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        self.SetBoneNormal(context.active_object, context.mode)
+        return {'FINISHED'}
+
+    def GetNormal(self,mesh_components):
+        # Get the component index
+        normals = []
+        num_verts = len(mesh_components)
+        sel = np.zeros(num_verts, dtype=np.bool)
+        mesh_components.foreach_get('select', sel)
+
+        for component in sel:
+            if component:
+                normal = mesh_components[component].normal
+                normals.append(normal)
+        print(normals)
+                
+        return normals
+
+    # get the normal of selected vertex or face or edge
+    def SetBoneNormal(self,obj, mode):
+        # Get the selected object
+        me = obj.data
+        print(me)
+        # Get the selected mode
+        if mode == 'VERTEX':
+            normals = self.GetNormal(me.vertices)
+        elif mode == 'FACE':
+            # Get the selected face
+            normals = self.GetNormal(me.polygons)
+        elif mode == 'EDGE':
+            # switch to Vertices mode
+            raise NotImplementedError("Edge mode not implemented")
+
+    # Convert the normal of a vertex to a rotational value
+    def NormalToRotation(self,normal):
+        # Get the normal of the component
+        normal = normal.normalized()
+        # Get the angle between the normal and the Z axis
+        angle = math.acos(normal.z)
+        # Get the rotation matrix
+        rot_mat = mathutils.Matrix.Rotation(angle, 4, 'Z')
+        # Get the rotation quaternion
+        rot_quat = rot_mat.to_quaternion()
+        # Get the rotation euler
+        rot_euler = rot_quat.to_euler()
+        
+        return rot_euler
+
+    def CreateBoneAlignedToNormal(self,armature,normal):
+        # Get the rotation euler
+        rot_euler = self.NormalToRotation(normal)
+        # Create the bone
+        bone = armature.edit_bones.new("Bone")
+        # Set the bone's rotation
+        bone.rotation_euler = rot_euler
+        # Set the bone's length
+        bone.tail = bone.head + mathutils.Vector((0,0,1))
+        # Set the bone's roll
+        bone.roll = 0
+        # Set the bone's parent
+        bone.parent = armature.edit_bones[0]
+        # Set the bone's name
+        bone.name = "Bone"
+        # Set the bone's head
+        bone.head = mathutils.Vector((0,0,0))
+        # Set the bone's tail
+        bone.tail = mathutils.Vector((0,0,1))
+        # Set the bone's roll
+        bone.roll = 0
+        # Set the bone's parent
+        bone.parent = armature.edit_bones[0]
+        # Set the bone's name
+
+
+
+
+def register():
+    bpy.utils.register_class(NormalToBone)
+
+def unregister():
+    bpy.utils.unregister_class(NormalToBone)
+
+if __name__ == "__main__":
+    register()
