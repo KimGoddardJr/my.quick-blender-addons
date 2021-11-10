bad5be1d1e36c855a6722259833ee8eef132c834
Patxi <patxi.aguirre@hslu.ch>
2021-11-10 10:36:14 +0100

    pie menu opt

--- normal_to_bone.py
+++ normal_to_bone.py
@@ -1,5 +1,11 @@
 import bpy
-from bpy.props import IntProperty, StringProperty, BoolProperty, FloatProperty, EnumProperty
+from bpy.props import (
+    IntProperty,
+    StringProperty,
+    BoolProperty,
+    FloatProperty,
+    EnumProperty,
+)
 import mathutils
 import math
 import numpy as np
@@ -13,12 +19,12 @@ bl_info = {
     "description": "creates a Bone in an Armature aligned to the normals of selected mesh component",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "Object"}
+    "category": "Object",
+}
 
 
-
-#Blender Enum Widget showing all available Armatures in the scene
-#Blender Menu Tab with bl_options
+# Blender Enum Widget showing all available Armatures in the scene
+# Blender Menu Tab with bl_options
 class ArmatureSelector(bpy.types.Panel):
 
     bl_idname = "HSLUCUSTOMPLUGINS_PT_plugins"
@@ -32,40 +38,36 @@ class ArmatureSelector(bpy.types.Panel):
         # Get the list of all Armatures in the scene
         armature_list = self.ArmatureLister
         bpy.types.Scene.armature_list = EnumProperty(
-        name="Armatures To Place Bones",
-        items=armature_list,
-        description="Armatures",
-        default="ALL",
+            name="Armatures To Place Bones",
+            items=armature_list,
+            description="Armatures",
+            default="ALL",
         )
-
 
     def draw(self, context):
 
         layout = self.layout
         layout.use_property_split = True
-        
+
         box = layout.box()
         box.label(text="Select Armature")
         box.prop(context.scene, "armature_list")
-
 
     def ArmatureLister(self):
         list_of_armatures = []
         # Get the list of all Armatures in the scene
         armature_list = bpy.data.armatures
         # Get the list of all Armatures in the scene
-        for i,armature in enumerate(armature_list):
-            new_armature = (armature.name,armature.name,armature.name, i)
+        for i, armature in enumerate(armature_list):
+            new_armature = (armature.name, armature.name, armature.name, i)
             list_of_armatures.append(new_armature)
             print(armature.name)
         return list_of_armatures
 
 
-
-
-
 class NormalToBone(bpy.types.Operator):
     """Tooltip"""
+
     bl_idname = "object.normal_to_bone"
     bl_label = "Normal to Bone"
 
@@ -75,91 +77,109 @@ class NormalToBone(bpy.types.Operator):
 
     def execute(self, context):
         self.SetBoneNormal(context.active_object, context.mode)
-        return {'FINISHED'}
+        return {"FINISHED"}
 
-    def GetLocationAndNormals(self,mesh,mesh_components):
+    def GetLocationAndNormals(self, mesh, mesh_components):
         # Get the component index
         loc_and_norms = []
         num_verts = len(mesh_components)
         sel = np.zeros(num_verts, dtype=np.bool)
-        mesh_components.foreach_get('select', sel)
+        mesh_components.foreach_get("select", sel)
 
         for component in sel:
             if component:
                 normal = mesh_components[component].normal
-                if mesh_components.bl_rna.name == 'Mesh Vertices':
+                if mesh_components.bl_rna.name == "Mesh Vertices":
                     location = mesh_components[component].co
-                elif mesh_components.bl_rna.name == 'Mesh Edges':
+                elif mesh_components.bl_rna.name == "Mesh Edges":
                     # the normal is the average of the two vertex normals of the edge
                     v1 = mesh_components[component].vertices[0]
                     v2 = mesh_components[component].vertices[1]
                     normal = (mesh.vertices[v1].normal + mesh.vertices[v2].normal) / 2
                     location = mesh.vertices[v1].co + mesh.vertices[v2].co
-                elif mesh_components.bl_rna.name == 'Mesh Polygons':
+                elif mesh_components.bl_rna.name == "Mesh Polygons":
                     location = mesh_components[component].center
-                
-                loc_and_norm = (location,normal)
-    
+
+                loc_and_norm = (location, normal)
+
         loc_and_norms.append(loc_and_norm)
         print(loc_and_norms)
-                
+
         return loc_and_norms
 
     # get the normal of selected vertex or face or edge
-    def SetBoneNormal(self,obj, mode):
+    def SetBoneNormal(self, obj, mode):
+        # Cur value of enum
+        armature = bpy.data.armatures[bpy.context.scene.armature_list]
         # Get the selected object
         me = obj.data
         print(me)
         # Get the selected mode
-        if mode == 'VERTEX':
-            pos_dirs = self.GetLocationAndNormals(me,me.vertices)
-        elif mode == 'FACE':
-            pos_dirs = self.GetLocationAndNormals(me,me.polygons)
-        elif mode == 'EDGE':
-            pos_dirs = self.GetLocationAndNormals(me,me.edges)
+        if mode == "VERTEX":
+            pos_dirs = self.GetLocationAndNormals(me, me.vertices)
+        elif mode == "FACE":
+            pos_dirs = self.GetLocationAndNormals(me, me.polygons)
+        elif mode == "EDGE":
+            pos_dirs = self.GetLocationAndNormals(me, me.edges)
 
-        
-        self.CreateBoneAlignedToNormal(armature,pos_dirs)
-
+        self.CreateBoneAlignedToNormal(armature, pos_dirs)
 
     # Convert the normal of a vertex to a rotational value
-    def NormalToRotation(self,normal):
+    def NormalToRotation(self, normal):
         # Get the normal of the component
         normal = normal.normalized()
         # Get the angle between the normal and the Z axis
         angle = math.acos(normal.z)
         # Get the rotation matrix
-        rot_mat = mathutils.Matrix.Rotation(angle, 4, 'Z')
+        rot_mat = mathutils.Matrix.Rotation(angle, 4, "Z")
         # Get the rotation quaternion
         rot_quat = rot_mat.to_quaternion()
         # Get the rotation euler
         rot_euler = rot_quat.to_euler()
-        
+
         return rot_euler
 
-    def CreateBoneAlignedToNormal(self,armature,pos_dirs):
+    def CreateBoneAlignedToNormal(self, armature, pos_dirs):
 
-        for pos,normal in pos_dirs:
+        for pos, normal in pos_dirs:
             # normal,rot_euler,angle = self.NormalToRotation(dir)
             bone = armature.edit_bones.new("Aligned-Bone")
             # Set the bone's length
             bone.head = pos
-            #bone.tail = pos + (bone.vector.normalized() * 0.5)
+            # bone.tail = pos + (bone.vector.normalized() * 0.5)
             bone.tail = pos + (normal.normalized() * 0.5)
 
-            #Rotate the whole bone aligned 
+            # Rotate the whole bone aligned
             # bone.matrix.rotate(rot_euler)
             # Set the bone's roll
             bone.roll = 0
 
 
+# Add the Normal To Bone Operator to a Pie Menu
+class NormalToBonePie(bpy.types.Menu):
+    bl_idname = "object.normal_to_bone_pie"
+    bl_label = "Normal to Bone"
+
+    def draw(self, context):
+        layout = self.layout
+        pie = layout.menu_pie()
+        pie.operator("object.normal_to_bone", text="Vertex", icon="VERTEXSEL")
+        pie.operator("object.normal_to_bone", text="Face", icon="FACESEL")
+        pie.operator("object.normal_to_bone", text="Edge", icon="EDGESEL")
+
+
+classes = [NormalToBone, ArmatureSelector, NormalToBonePie]
 
 
 def register():
-    bpy.utils.register_class(NormalToBone)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
 
 def unregister():
-    bpy.utils.unregister_class(NormalToBone)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
 
 if __name__ == "__main__":
     register()
