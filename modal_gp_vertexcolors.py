65d8792f23e87ec4752db2e9ada9d4d62bc49c68
Patxi <patxi.aguirre@hslu.ch>
2021-11-11 10:23:27 +0100

    less convoluted structure

---
+++ modal_gp_vertexcolors.py
@@ -0,0 +1,1 @@
+### The idea is a to dynamically assign the color of the surface that is being painted on to the vertex colors of the grease pencil stroke.
--- reset_delta_transforms.py
+++ reset_delta_transforms.py
@@ -1,3 +1,5 @@
+# the general idea is to reset the deltas of all objects to their original values
+
 import bpy
 
 
@@ -10,45 +12,45 @@ bl_info = {
     "description": "reset the delta to 0 on many objects",
     "wiki_url": "",
     "tracker_url": "",
-    "category": "Object"}
-
+    "category": "Object",
+}
 
 
 class reset_deltas(bpy.types.Operator):
     """Cleanup your object deltas"""
+
     bl_idname = "transform.reset_deltas"
     bl_label = "Cleans up all the deltas"
-    bl_options = {'REGISTER', 'UNDO'}
-    
-    def execute(self,context):
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
         for obj in bpy.context.selected_objects:
-            #raw location data
-            bpy.ops.object.transforms_to_deltas(mode='ALL')
+            # raw location data
+            bpy.ops.object.transforms_to_deltas(mode="ALL")
 
             obj_deltaL = obj.delta_location
             obj_deltaR = obj.delta_rotation_euler
             obj_deltaS = obj.delta_scale
 
-            # Assign new values to transforms as tuples           
+            # Assign new values to transforms as tuples
             obj.location = obj_deltaL
-            obj.delta_location = (0,0,0)
+            obj.delta_location = (0, 0, 0)
             obj.rotation_euler = obj_deltaR
-            obj.delta_rotation_euler = (0,0,0)
+            obj.delta_rotation_euler = (0, 0, 0)
             obj.scale = obj_deltaS
-            obj.delta_scale = (1,1,1)
+            obj.delta_scale = (1, 1, 1)
 
-        return {'FINISHED'}
-
+        return {"FINISHED"}
 
 
 def menu_func(self, context):
-    self.layout.operator(reset_deltas.bl_idname, text = "Reset Deltas")
-
+    self.layout.operator(reset_deltas.bl_idname, text="Reset Deltas")
 
 
 def register():
     bpy.utils.register_class(reset_deltas)
     bpy.types.VIEW3D_MT_object_apply.append(menu_func)
+
 
 def unregister():
     bpy.utils.unregister_class(reset_deltas)
